79b0d7820d8b610557c7766220f7fc1367ab607d
magic-zhu <18817313320@163.com>
2022-05-24 22:42:58 +0800

    update

--- src/core/canvasToWebglCoordinate.ts
+++ src/core/canvasToWebglCoordinate.ts
@@ -1,4 +1,13 @@
-
-const canvasToWebglCoordinate = (x, y) => {
-
-};
+/**
+ * 归一化坐标系
+ * @param x - 坐标点
+ * @param y - 坐标点
+ * @param canvasWitdh - 画布宽度
+ * @param canvasHeight - 画布高度
+ */
+const canvasToWebglCoordinate = (
+  x: number,
+  y: number,
+  canvasWitdh: number,
+  canvasHeight: number
+) => {}
--- src/shader/points.ts
+++ src/shader/points.ts
@@ -1,15 +1,15 @@
 export const pointsVertexShader = () => {
-  return `
+  return /*glsl*/ `
     attribute vec4 a_position;
     void main() {
       gl_Position = a_position;
     }
-  `;
-};
+  `
+}
 export const pointsFragmentShader = (r: number, g: number, b: number) => {
-  return `
+  return /*glsl*/ `
       void main(){
         gl_FragColor = vec4(${r}, ${g}, ${b}, 1.0);
       }
-  `;
-};
+  `
+}
